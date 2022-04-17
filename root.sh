bd03d9627b379ad1c4707749a341fa7754d754f1
Raphaele Salvatore Licciardo <lira1011@hs-karlsruhe.de>
2022-04-17 10:21:36 +0200

    bspwm, polybar und installation changes

--- root.sh
+++ root.sh
@@ -17,6 +17,7 @@ apt update
 # Add base packages
 apt install unzip picom bspwm polybar sddm rofi kitty thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg -y
 apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
+apt install variety chromium feh 
 
 # Download Nordic Theme
 cd /usr/share/themes/
--- user.sh
+++ user.sh
@@ -11,3 +11,9 @@ cd "$HOME/build"
 git clone https://github.com/vinceliuice/Layan-cursors
 cd Layan-cursors
 sudo ./install.sh
+
+# Background 
+wget "https://raw.githubusercontent.com/RaphaeleL/DebianSetup/main/background.jpg"
+feh --bg-fill background.jpg
+
+
